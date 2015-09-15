3a38c7b40b096aa113999dee2de7e4c6eedef048
saymtf <mfenton@csumb.edu>
2015-09-15 09:12:16 -0700

    Calc Runthrough

---
+++ CalculatorSwift/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  CalculatorSwift
+//
+//  Created by Mitchell Fenton on 9/1/15.
+//  Copyright (c) 2015 saymtf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false;
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!; // let is a constant, ! gets the string of button
+        //println("Digit = \(digit)"); // print
+        if( userIsInTheMiddleOfTypingANumber) {
+        display.text = display.text! + digit; // need !, bc its an optional type need to unwrap
+        }else {
+            display.text = digit;
+            userIsInTheMiddleOfTypingANumber = true;
+        }
+    }
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!;
+        if(userIsInTheMiddleOfTypingANumber) {
+            enter();
+        }
+        
+        switch operation {
+        case "X":
+            // performs a quick closure function
+            performOperation { $0 * $1 };
+        case "/":
+            // performs a quick closure function
+            performOperation { $1 / $1 };
+        case "+":
+            // performs a quick closure function
+            performOperation { $0 + $1 };
+        case "-":
+            // performs a quick closure function
+            performOperation { $1 - $0 };
+        case "√":
+            // performs a quick closure function
+            performOperation { sqrt($0) };
+        default: break;
+        }
+    }
+    
+    // Takes two doubles and returns a double
+    func performOperation(operation: (Double, Double) -> Double) {
+        if(operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast());
+            enter();
+        }
+    }
+
+    // Takes two doubles and returns a double
+    func performOperation(operation: (Double) -> Double) {
+        if(operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast());
+            enter();
+        }
+    }
+    
+    //var operandStack: Array<Double> = Array<Double>();
+    var operandStack = Array<Double>(); // infers that it's an Array
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false;
+        operandStack.append(displayValue); // call the computed prop.
+        println("operandStack = \(operandStack)");
+        
+    }
+    
+    //computed properties
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set {
+            display.text = "\(newValue)"; //converts newValue to string
+            userIsInTheMiddleOfTypingANumber = false;
+        }
+    }
+    
+}
+
---
+++ CalculatorSwiftTests/CalculatorSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  CalculatorSwiftTests.swift
+//  CalculatorSwiftTests
+//
+//  Created by Mitchell Fenton on 9/1/15.
+//  Copyright (c) 2015 saymtf. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CalculatorSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
